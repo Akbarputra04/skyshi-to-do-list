84f2db9647aac21173f5906d44061756012d5a85
akbar putra <ap975527@gmail.com>
2023-01-05 14:24:03 +0700

    melengkapi testing prop

--- src/components/EmptyState/index.js
+++ src/components/EmptyState/index.js
@@ -1,6 +1,6 @@
 const index = ({image, text, cy}) => {
     return (
-        <div className="flex-1 flex flex-col items-center justify-center gap-6" cy={cy}>
+        <div className="flex-1 flex flex-col items-center justify-center gap-6" data-cy={cy}>
             <img src={image} alt="empty state" />
             <p className="font-semibold text-center">{text}</p>
         </div>
