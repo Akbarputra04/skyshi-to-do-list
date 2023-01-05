999554d4a8b65cb04defe7baa2ff58fb59cdcb60
akbar putra <ap975527@gmail.com>
2023-01-05 14:42:20 +0700

    melengkapi testing prop

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -39,11 +39,11 @@ const TodoItem = ({id, title, priority, active, setAlertShow, setIsLoading}) => 
                     <input type="checkbox" className="checked:bg-primary md:w-5 md:h-5" checked={!active} onChange={changeStatus} data-cy="todo-item-checkbox" />
                     <div className={`w-[5px] h-[5px] md:w-[9px] md:h-[9px] rounded-full ${priorityConfig[priority]}`} data-cy="todo-item-priority-indicator"></div>
                     <div className={`flex items-center gap-2 md:gap-6 font-medium text-sm md:text-lg ${!active ? 'line-through' : ''} text-[#c4c4c4]`}>
-                        <span data-cy="todo-title">{title}</span>
+                        <span data-cy="todo-item-title">{title}</span>
                         <BsPencil color="#888888" data-cy="todo-item-edit-button" onClick={e => {e.preventDefault(); setShow(true)}} />
                     </div>
                 </div>
-                <BsTrash color="#888888" data-cy="todo-item-delete-button" onClick={e => {e.preventDefault(); setDeleteShow(true)}} data-cy="todo-item-delete-button" />
+                <BsTrash color="#888888" onClick={e => {e.preventDefault(); setDeleteShow(true)}} data-cy="todo-item-delete-button" />
             </div>
             {/* modal */}
             <FormModal show={show} onClose={() => setShow(false)} id={id} data={{title, priority}} setIsLoading={setIsLoading} />
